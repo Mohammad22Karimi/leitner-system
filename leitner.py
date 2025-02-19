184e7b94972befa4df169c269d1793b9d75ef140
Mohammad22Karimi <mokarimi22.4@gmail.com>
2025-02-19 19:24:05 +0330

    define the leitner boxes and their review intervals. and file to store.

--- leitner.py
+++ leitner.py
@@ -1,3 +1,13 @@
 import json
 import os
 from datetime import timedelta
+
+DATA_FILE = "leitner_box.json"
+
+BOXES = {
+    1: timedelta(days=1), # box 1 : review every 1 day
+    2: timedelta(days=3), # box 2 : review every 3 day
+    3: timedelta(days=7), # box 3 : review every 7 day
+    4: timedelta(days=14), # box 4 : review every 14 day
+    5: timedelta(days=30), # box 5 : review every 30 day
+}
